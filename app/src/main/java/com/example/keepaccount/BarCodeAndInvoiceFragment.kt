985a5a13cde4dfae452211f5e8f7ac41ff0de754
Alan <alan282403@gmail.com>
2026-01-13 16:28:25 +0800

    [MOD] 更正TAB字

--- app/src/main/java/com/example/keepaccount/BarCodeAndInvoiceFragment.kt
+++ app/src/main/java/com/example/keepaccount/BarCodeAndInvoiceFragment.kt
@@ -10,7 +10,7 @@ import com.example.keepaccount.ui.adapter.BarCodeAndInvoiceFragmentViewPagerAdap
 import com.google.android.material.tabs.TabLayoutMediator
 
 class BarCodeAndInvoiceFragment : Fragment() {
-    private val tabTitleArray = arrayOf("載具設定", "發票兌獎")
+    private val tabTitleArray = arrayOf("載具設定", "發票對獎")
     private var _binding: FragmentBarcodeInvoiceBinding? = null
     private val binding get() = _binding!!
 
